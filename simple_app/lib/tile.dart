cf84914c603c70b273d94422521efda508f4b148
Kazumasa Wakamori <wakamori.kazumasa.17@shizuoka.ac.jp>
2020-07-26 17:12:37 +0900

    Update talk scene

---
+++ simple_app/lib/tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class Tile extends StatelessWidget {
+  IconData icon;
+  String username;
+  String message;
+
+  Tile({IconData icon, String username, String message}) {
+    this.icon = icon;
+    this.username = username;
+    this.message = message;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.20,
+      child: Container(
+        color: Colors.white,
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Icon(this.icon),
+            backgroundColor: Colors.pink,
+          ),
+          title: Text(this.username),
+          subtitle: Text(this.message),
+          onTap: () => {},
+        )
+      ),
+      actions: <Widget>[
+        IconSlideAction(
+          color: Colors.blue,
+            icon: Icons.flash_off,
+            onTap: () => {},
+        ),
+        IconSlideAction(
+          color: Colors.indigo,
+            icon: Icons.volume_off,
+          onTap: () => {},
+        ),
+      ],
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          color: Colors.black45,
+          iconWidget: Text(
+            "非表示",
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () => {},
+        ),
+        IconSlideAction(
+          color: Colors.red,
+          iconWidget: Text(
+            "削除",
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () => {},
+        )
+      ],
+    );
+  }
+}
