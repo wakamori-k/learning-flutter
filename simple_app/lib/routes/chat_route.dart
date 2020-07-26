5c3518670dd9362a827d023ba225bdd2ebd050ff
Kazumasa Wakamori <wakamori.kazumasa.17@shizuoka.ac.jp>
2020-07-26 17:41:44 +0900

    Add char footer

--- simple_app/lib/routes/chat_route.dart
+++ simple_app/lib/routes/chat_route.dart
@@ -38,6 +38,36 @@ class Chat extends StatelessWidget {
       ),
       body: Center(child: Text("Chat"),),
       backgroundColor: Colors.cyan,
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.camera_alt),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.photo),
+              onPressed: () => {},
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Aa'
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.mic),
+              onPressed: () => {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
